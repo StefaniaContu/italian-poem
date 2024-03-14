b7b6e02e8edee5da5c9b20b713a0d3c1e58c752d
StefaniaContu <stefania_contu@yahoo.com>
2024-03-14 13:47:08 +0000

    Added the basic JavaScript and Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generatePoem(event) {
+  event.preventDefault();
+  alert("Generating Poem");
 
+  new Typewriter("#poem", {
+    strings: ["Si sta come d'inverno sugli alberi le foglie"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
