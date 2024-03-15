a55811797d47f893cf10f229f6d1cfc2cb145f23
StefaniaContu <stefania_contu@yahoo.com>
2024-03-15 09:31:15 +0000

    Removed the console.log

--- src/index.js
+++ src/index.js
@@ -23,10 +23,6 @@ function generatePoem(event) {
   poemElement.innerHTML = `<div class="generating">
       ⏳ Generating an Italian poem about ${instructionsInput.value}</div>`;
 
-  console.log("Generating poem");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
-
   axios.get(apiUrl).then(displayPoem);
 }
 
