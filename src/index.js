c97264ec1eeb3cdecb7f35facd6e626e7c876425
StefaniaContu <stefania_contu@yahoo.com>
2024-03-15 09:28:57 +0000

    Improved html, added animation to css

--- src/index.js
+++ src/index.js
@@ -18,11 +18,16 @@ function generatePoem(event) {
     "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br/>. Make sure to follow the user instructions. Do not include a title. Sign the poem with 'SheCodes AI' inside a <strong/> element at the end of the poem";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating poem");
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">
+      ⏳ Generating an Italian poem about ${instructionsInput.value}</div>`;
 
-  axios.get(apiUrl).then(displayPoem);
+  console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
